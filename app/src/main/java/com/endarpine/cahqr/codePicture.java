7805e3684a4f1c1a660fff59c067d63e48046ef5
gratipine <gratipine@protonmail.com>
2017-01-25 10:19:02 +0000

    QR code shows up on screen

--- app/src/main/java/com/endarpine/cahqr/codePicture.java
+++ app/src/main/java/com/endarpine/cahqr/codePicture.java
@@ -2,18 +2,26 @@ package com.endarpine.cahqr;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.endarpine.cahqr.Logic.Facade;
 import com.google.zxing.WriterException;
+
+import android.view.View;
+
 
 public class codePicture extends AppCompatActivity {
 
     private static String message;
     private static final String ALERT_TEXT = "Uh oh, something went wrong";
-    //todo: the message should be displayed together with the picture
+    public static Bitmap QRcode;
+
+    //todo: fix layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +30,14 @@ public class codePicture extends AppCompatActivity {
         //getting the contents of the user input
         Intent intent = getIntent();
         message = intent.getStringExtra(addCode.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.textView_addedCodeText);
+        textView.setText(message);
 
         //encoding
         try {
-            Bitmap code = Facade.createQR(message);
+            QRcode = Facade.createQR(message);
+            ImageView codeImage = (ImageView)findViewById(R.id.imageView_addedCodeImage);
+            codeImage.setImageBitmap(QRcode);
         } catch (WriterException e) {
             //todo:handle exception
             e.printStackTrace();
@@ -33,7 +45,10 @@ public class codePicture extends AppCompatActivity {
             //todo: handle this
             e.printStackTrace();
         }
-        //todo:add a container for the QR code in the layout
+    }
 
+        public void saveQRtoDevice (View view){
+            String description = "QR code created through CAHQR";
+            MediaStore.Images.Media.insertImage(getContentResolver(),QRcode,message,description);
     }
 }
